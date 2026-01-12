48f21a4f0079bcfb484783dacfc19c96d7c5e02c
arnav-py9 <arnavsingh.gg@gmail.com>
2026-01-12 22:20:36 +0530

    get api to fetch user finance

--- app/endpoints/users_finances_endpoint.py
+++ app/endpoints/users_finances_endpoint.py
@@ -8,14 +8,13 @@ router = APIRouter()
 @router.get("/")
 def get_user_finances(user_id: str = Header(None)):
     if not user_id:
-        raise HTTPException(detail="User ID missing")
+        raise HTTPException(detail="User ID missing", status_code=400)
 
     finances = get_collection("users_finances")
-
-    data = finances.find_one({"user_id": ObjectId(user_id)})
+    data = finances.find_one({"user_id": user_id})
 
     if not data:
-        raise HTTPException(detail="User finance data not found")
+        raise HTTPException(detail="User finance data not found", status_code=404)
 
     data["_id"] = str(data["_id"])
     data["user_id"] = str(data["user_id"])
