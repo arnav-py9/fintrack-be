4e6e35db28bd4d3d20ed2a6aff16274460ce6314
arnav-py9 <arnavsingh.gg@gmail.com>
2026-01-15 22:14:21 +0530

    put api

--- app/endpoints/users_finances_endpoint.py
+++ app/endpoints/users_finances_endpoint.py
@@ -20,3 +20,22 @@ def get_user_finances(user_id: str = Header(None)):
     data["user_id"] = str(data["user_id"])
 
     return data
+
+
+@router.put("/")
+def update_user_finances(data: dict, user_id: str = Header(None)):
+    if not user_id:
+        raise HTTPException(detail="User ID missing", status_code=400)
+
+    finances = get_collection("users_finances")
+
+    # Remove _id if present to avoid Mongo write errors
+    if "_id" in data:
+        del data["_id"]
+
+    result = finances.update_one({"user_id": user_id}, {"$set": data})
+
+    if result.matched_count == 0:
+        raise HTTPException(detail="User finance data not found", status_code=404)
+
+    return data
